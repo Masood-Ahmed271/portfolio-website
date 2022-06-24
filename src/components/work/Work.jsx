f6a17029586648e43daa9443a1976a0f68372642
Masood-Ahmed271 <mangimasood2000@gmail.com>
2022-06-24 18:25:26 +0800

    Added links for the workspaces

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -24,6 +24,7 @@ const data = [
     description:
       "Worked as a Stem Course Developer where I got an opportunity to develop projects such as Cospaces and Unity games. Apart from that got a chance to test various hardwares and develop drone and Virtual Reality programs as well.",
     image: PNL,
+    link:"https://www.pnledu.org/",
   },
 
   {
@@ -32,6 +33,7 @@ const data = [
     description:
       "Worked as a Software Quality Assurance Intern where I got an opportunity to do 'Smoke-Testing' of the stagingwp website of Weavatools. Along with this, monitored and tested the website using Apache JMeter and used Cypress to create automated test cases to test the website and chrome extension.",
     image: WEAVA,
+    link:"https://www.weavatools.com/",
   },
   {
     companyId: 3,
@@ -39,6 +41,7 @@ const data = [
     description:
       "Worked as a Student Research Assitant where I was involved in the research for finding mathematical proof for a different version of CHSH game, in both classical and quantum models.",
     image: HKUCS,
+    link:"https://www.cs.hku.hk/",
   },
   {
     companyId: 4,
@@ -46,6 +49,7 @@ const data = [
     description:
       "Worked as a Technical Team Member where I had to work closely with the team to execute day-to-day tasks such as testing various components both, software and hardware. Along with that, developed and contributed to the presentations for the pitching team at Breed.",
     image: BREED,
+    link:"https://innowings.engg.hku.hk/robofish/",
   },
 
   {
@@ -54,6 +58,7 @@ const data = [
     description:
       "Working and collaborating with TechNine developers to develop software solutions for webapps and mobile apps. Along with that, served as bridge between DocPro's management and DocPro's developers.",
     image: DOCPRO,
+    link:"https://docpro.com/",
   },
 ];
 
@@ -64,10 +69,10 @@ const Work = () => {
       <h2>Work Experience</h2>
       <Swiper className="container work_container" modules = {[Pagination]} spaceBetween = {40} slidersPerView={1} pagination={{clickable: true}}>
         {
-        data.map(({ companyId, name, description, image }) => {
+        data.map(({ companyId, name, description, image, link }) => {
           return(<SwiperSlide key={companyId} className="work">
             <div className="company_image">
-              <img src={image} alt="Company's Logo" />
+              <a href={link} target="_blank"><img src={image} alt="Company's Logo" /></a>
             </div>
             <h5 className="compnay_name">{name}</h5>
             <small className="Compnay_description">
