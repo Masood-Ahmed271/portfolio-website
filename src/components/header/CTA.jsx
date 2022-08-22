ab3a41bf46a1931e515796cb4f8a8abbf2ba77e6
Masood-Ahmed271 <mangimasood2000@gmail.com>
2022-08-22 16:14:33 +0800

    Added an alert box

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/CV.pdf'
 const CTA = () => {
   return (
     <div className="cta">
-        <a href={CV} className='btn' target='_blank'>Download CV</a>
+        <a onClick={() =>{ alert("Please Note: The CV that you would be viewing right now is not the updated/latest CV. Thank You!")}} href={CV} className='btn' target='_blank'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Feel Free To Reach Out</a>
     </div>
   )
